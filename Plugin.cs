fba68ad4872799023a10c9c2ee05569d07ec84d2
ErenEksen <97560144+ErenEksen@users.noreply.github.com>
2024-03-04 18:51:25 +0200

    First Release
    Green Screen Moon and Scrap for Youtubers.

---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace GreenScreenMoon
+{
+    [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        const string PLUGIN_GUID = "Axis.GreenScreenMoon";
+        const string PLUGIN_NAME = "Green Screen Moon";
+        const string PLUGIN_VERSION = "1.0.0b-1";
+        internal ManualLogSource mls;
+        private static Plugin Instance;
+        
+        private void Awake()
+        {
+            if(Instance == null) Instance = this;
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(PLUGIN_GUID);
+            mls.LogInfo($"{PLUGIN_NAME} loaded successfully!");
+        }
+    }
+}
---
+++ obj/Debug/net35/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace GreenScreenMoon
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "GreenScreenMoon";
+        public const string PLUGIN_NAME = "GreenScreenMoon";
+        public const string PLUGIN_VERSION = "1.0.0b-1";
+    }
+}
